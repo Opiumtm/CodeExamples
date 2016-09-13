731fb2c5d26989b27e0f422146c1dd5fd298b3e4
Opiumtm <opium_tm@live.ru>
2016-09-13 19:27:27 +0300

    Memory leak test

---
+++ BufferMemoryLeak/BufferMemoryLeak/LeakyExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Buffer = Windows.Storage.Streams.Buffer;
+
+namespace BufferMemoryLeak
+{
+    public static class LeakyExample
+    {
+        public static async Task<ulong> Run()
+        {
+            var source = await PrepareFileSource();
+            using (var memStream = new InMemoryRandomAccessStream())
+            {
+                using (var sourceStr = await source.OpenReadAsync())
+                {
+                    await CopyStreamAsync(sourceStr, memStream);
+                    return memStream.Size;
+                }
+            }
+        }
+
+        public static async Task CopyStreamAsync(this IInputStream src, IOutputStream outStream, uint bufferSize = 16384)
+        {
+            IBuffer buf2;
+            do
+            {
+                var buf = new Buffer(bufferSize);
+                buf2 = await src.ReadAsync(buf, bufferSize, InputStreamOptions.None);
+                if (buf2.Length > 0)
+                {
+                    await outStream.WriteAsync(buf2);
+                }
+            } while (buf2.Length > 0);
+        }
+
+        public static async Task<StorageFile> PrepareFileSource()
+        {
+            var file = await ApplicationData.Current.LocalFolder.CreateFileAsync("example.dat", CreationCollisionOption.OpenIfExists);
+            var p = await file.GetBasicPropertiesAsync();
+            if (p.Size == 0)
+            {
+                var b = new byte[1024];
+                for (var j = 0; j < b.Length; j++)
+                {
+                    b[j] = (byte)(j % 256);
+                }
+                using (var str = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    using (var wr = new DataWriter(str))
+                    {
+                        for (int i = 0; i < 4096; i++)
+                        {
+                            wr.WriteBytes(b);
+                            if (i%5 == 4)
+                            {
+                                await wr.FlushAsync();
+                            }
+                        }
+                        await wr.FlushAsync();
+                    }
+                }
+            }
+            return file;
+        }
+    }
+}
---
+++ BufferMemoryLeak/BufferMemoryLeak/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BufferMemoryLeak
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UpdateAppMemoryUsage()
+        {
+            var rep = MemoryManager.GetAppMemoryReport();
+            var privateMb = rep.PrivateCommitUsage/(1024.0 * 1024.0);
+            var totalMb = rep.TotalCommitUsage / (1024.0 * 1024.0);
+            PrivateCommit = $"{privateMb:F2} Mb";
+            TotalCommit = $"{totalMb:F2} Mb";
+        }
+
+        public static readonly DependencyProperty PrivateCommitProperty = DependencyProperty.Register(
+            "PrivateCommit", typeof (string), typeof (MainPage), new PropertyMetadata(default(string)));
+
+        public string PrivateCommit
+        {
+            get { return (string) GetValue(PrivateCommitProperty); }
+            set { SetValue(PrivateCommitProperty, value); }
+        }
+
+        public static readonly DependencyProperty TotalCommitProperty = DependencyProperty.Register(
+            "TotalCommit", typeof (string), typeof (MainPage), new PropertyMetadata(default(string)));
+
+        public string TotalCommit
+        {
+            get { return (string) GetValue(TotalCommitProperty); }
+            set { SetValue(TotalCommitProperty, value); }
+        }
+
+        private async void RunButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            RunButton.IsEnabled = false;
+            for (int i = 0; i < 10; i++)
+            {
+                await LeakyExample.Run();
+                UpdateAppMemoryUsage();
+            }
+            RunButton.IsEnabled = true;
+        }
+
+        private void GetStatsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            UpdateAppMemoryUsage();
+        }
+
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            UpdateAppMemoryUsage();
+        }
+
+        private void GcButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            GC.Collect(2, GCCollectionMode.Forced, true);
+            GC.WaitForPendingFinalizers();
+            GC.Collect(2, GCCollectionMode.Forced, true);
+            UpdateAppMemoryUsage();
+        }
+    }
+}
