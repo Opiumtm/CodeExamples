e915960e24c627be3b077540b9b2b26ebca40a0f
Opiumtm <opium_tm@live.ru>
2017-04-20 15:01:57 +0300

    Nuget packages update

--- ReflectionBenchmark/ReflectionBenchmark/App.xaml.cs
+++ ReflectionBenchmark/ReflectionBenchmark/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Data.Sqlite.Internal;
 
 namespace ReflectionBenchmark
 {
@@ -30,6 +31,7 @@ namespace ReflectionBenchmark
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            SqliteEngine.UseWinSqlite3();
         }
 
         /// <summary>
